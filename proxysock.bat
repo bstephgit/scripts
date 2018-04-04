2fb502465b46a8bb7c09a619cfdbb4525561fda6
bstephgit <tcn75323@gmail.com>
2018-04-04 11:53:26 +0200

    add proxyscock.bat script

---
+++ proxysock.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+setlocal
+
+:loop
+if x%1 equ x goto done
+set param=%1
+if %param:~0,1% equ - goto checkParam
+:paramError
+echo Parameter error: %1 
+
+:next
+shift /1
+goto loop
+
+:checkParam
+if "%1" equ "-f" goto input_file
+if "%1" equ "-o" goto output_file
+goto paramError
+
+:input_file
+    shift /1
+    set INPUT_FILE=%1
+    goto next
+
+:output_file
+    shift /1
+    set OUTPUT_FILE=%1
+    goto next
+:done
+IF "%OUTPUT_FILE%"=="" SET OUTPUT_FILE=proxys_socks5.txt
+IF EXIST %INPUT_FILE% GOTO ADDPREFIX
+ECHO ON
+ECHO ERROR => INPUT FILE (-f FILE) NOT SPECIFIED OR NOT EXIST
+@ECHO OFF
+GOTO :EOF
+
+:ADDPREFIX
+IF EXIST %OUTPUT_FILE% DEL /Q %OUTPUT_FILE%
+FOR /F %%i IN (%INPUT_FILE%) DO ECHO socks5://%%i >> %OUTPUT_FILE%
+START NOTEPAD.EXE %OUTPUT_FILE%
